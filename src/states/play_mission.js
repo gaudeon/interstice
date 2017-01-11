08ad056a1d25889cc804eb47c104dc718f6e78a8
DizzyDogg <jeffthetenor@yahoo.com>
2017-01-11 09:36:42 -0700

    add player image for looks

--- src/states/play_mission.js
+++ src/states/play_mission.js
@@ -15,12 +15,16 @@ App.PlayMissionState = (function () {
     };
 
     fn.prototype.preload = function () {
-        // console.log(this.game);
         this.load.image('space', 'assets/images/spaceBGDarkPurple.png');
+        this.load.image('player', 'assets/images/playerShipO.png');
     };
 
     fn.prototype.create = function () {
         this.background = this.add.tileSprite(0, 0, this.game.world.width * 2, this.game.world.height * 2, 'space');
+
+        this.player = this.add.sprite(this.game.world.width / 2, this.game.world.height / 2, 'player');
+        this.player.anchor.setTo(0.5);
+        this.player.scale.setTo(0.5);
     };
 
     return fn;
