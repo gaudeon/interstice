a493558c99549eaee7b89bf7242e0d75dbb95b85
DizzyDogg <jeffthetenor@yahoo.com>
2017-01-11 09:17:54 -0700

    apply background image

--- src/states/play_mission.js
+++ src/states/play_mission.js
@@ -15,9 +15,12 @@ App.PlayMissionState = (function () {
     };
 
     fn.prototype.preload = function () {
+        // console.log(this.game);
+        this.load.image('space', 'assets/images/spaceBGDarkPurple.png');
     };
 
     fn.prototype.create = function () {
+        this.background = this.add.tileSprite(0, 0, this.game.world.width * 2, this.game.world.height * 2, 'space');
     };
 
     return fn;
