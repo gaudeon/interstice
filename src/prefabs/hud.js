e8f5463bc26a10e68e3af2a61c7da9a389296954
Daniel Blankenship <dblankenship@10-200-36-219.employee-macbook.bluehost.com>
2017-01-11 14:32:07 -0700

    added background healthbar

--- src/prefabs/hud.js
+++ src/prefabs/hud.js
@@ -9,16 +9,32 @@ App.HUD = (function () {
     };
 
     fn.prototype.loadAssets = function () {
-        this.game.load.image('bluePanel', 'assets/images/hud_elements/metalPanel_blue.png');
+        this.game.load.image('healthbarBgLeft', 'assets/images/hud_elements/barHorizontal_shadow_left.png');
+        this.game.load.image('healthbarBgMid', 'assets/images/hud_elements/barHorizontal_shadow_mid.png');
+        this.game.load.image('healthbarBgRight', 'assets/images/hud_elements/barHorizontal_shadow_right.png');
+        this.game.load.image('healthbarGreenLeft', 'assets/images/hud_elements/barHorizontal_green_left.png');
+        this.game.load.image('healthbarGreenMid', 'assets/images/hud_elements/barHorizontal_green_mid.png');
+        this.game.load.image('healthbarGreenRight', 'assets/images/hud_elements/barHorizontal_green_right.png');
     };
 
     fn.prototype.displayHUD = function () {
         this.group = this.game.add.group();
-        this.fixedToCamera = true;
 
-        this.bluePanel = this.game.add.sprite(0, 0, 'bluePanel');
-        this.bluePanel.fixedToCamera = true;
-        this.group.add(this.bluePanel);
+        var healthbarXPos = 5;
+        var healthbarYPos = 5;
+
+        this.healthbarBgLeft = this.game.add.sprite(healthbarXPos, healthbarYPos, 'healthbarBgLeft');
+        this.healthbarBgLeft.fixedToCamera = true;
+        this.group.add(this.healthbarBgLeft);
+
+        this.healthbarBgMid = this.game.add.sprite(healthbarXPos + this.healthbarBgLeft.width, healthbarYPos, 'healthbarBgMid');
+        this.healthbarBgMid.fixedToCamera = true;
+        this.healthbarBgMid.width = 400;
+        this.group.add(this.healthbarBgMid);
+
+        this.healthbarBgRight = this.game.add.sprite(healthbarXPos + this.healthbarBgMid.width + this.healthbarBgLeft.width, healthbarYPos, 'healthbarBgRight');
+        this.healthbarBgRight.fixedToCamera = true;
+        this.group.add(this.healthbarBgRight);
     };
 
     return fn;
