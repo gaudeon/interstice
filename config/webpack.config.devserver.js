b95188bf3929921aac046f8502e2c1dc94df8cbb
hookbot <hookbot@gmail.com>
2018-03-03 14:13:54 -0500

    Allow ENV "PORT" to override listen port

--- config/webpack.config.devserver.js
+++ config/webpack.config.devserver.js
@@ -1,6 +1,10 @@
 const path = require('path');
 const webpack = require('webpack');
 
+// Listen port
+const PORT = process.env.PORT || 8080;
+
+
 // Load base config
 const baseConfig = require('./webpack.config.base');
 
@@ -10,6 +14,9 @@ const config = Object.create(baseConfig);
 config.devtool = 'cheap-source-map';
 
 config.devServer = {
+    hot: true,
+    host: "0.0.0.0",
+    port: PORT,
     contentBase: path.resolve(__dirname, '../src'),
     watchContentBase: true
 };
